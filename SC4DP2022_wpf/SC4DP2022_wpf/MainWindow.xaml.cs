5904c01315791a434463891180df35b091a4e64b
noah-severyn <50927852+noah-severyn@users.noreply.github.com>
2021-12-12 17:43:32 -0500

    list all folders + subfolders

--- SC4DP2022_wpf/SC4DP2022_wpf/MainWindow.xaml.cs
+++ SC4DP2022_wpf/SC4DP2022_wpf/MainWindow.xaml.cs
@@ -20,55 +20,124 @@ namespace SC4DP2022_wpf {
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
 	public partial class MainWindow : Window {
-		private string activeDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\SimCity 4\\Plugins";
-		private string destinationDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\SimCity 4\\Plugins\\Plugins_Compressed";
+		private string activeDirectoryPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\SimCity 4\\Plugins";
+		private string destinationDirectoryPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\SimCity 4\\Plugins\\Plugins_Compressed";
+		private bool resurseIntoSubfolders = true;
 
 		public MainWindow() {
 			InitializeComponent();
 
 			// Set default folders on startup + fill listbox with items in default folder
-			SourceDirectory.Text = activeDirectory;
-			DestinationDirectory.Text = destinationDirectory;
-			FillFolderList(activeDirectory);
-		}
-
-		private void BrowseSourceDirectory_Click(object sender, RoutedEventArgs e) {
-
-			// Folder picker dialog box
-			// https://stackoverflow.com/a/41511598
-			CommonOpenFileDialog dialog = new CommonOpenFileDialog {
-				InitialDirectory = activeDirectory,
-				IsFolderPicker = true
-			};
-			if (dialog.ShowDialog() == CommonFileDialogResult.Ok) {
-				//MessageBox.Show("You selected: " + dialog.FileName);
-				SourceDirectory.Text = dialog.FileName;
-				activeDirectory = dialog.FileName;
-			}
-
-			FillFolderList(activeDirectory);
+			SourceDirectory.Text = activeDirectoryPath;
+			DestinationDirectory.Text = destinationDirectoryPath;
+			PopulateFolderListbox(activeDirectoryPath);
 		}
 
 
 
 		/// <summary>
-		/// Lists all folders in the specified path in list_Folders listBox
+		/// Opens a folder picker dialog to choose the source directory. The source directory determines the folders to be shown in FolderList.
 		/// </summary>
-		/// <param name="path"></param>
-		private void FillFolderList(string path) {
-			string[] folders = Directory.GetDirectories(path);
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void BrowseSourceDirectory_Click(object sender, RoutedEventArgs e) {
 
-			// Remove all existing list items
+			// Folder picker dialog box
+			// https://stackoverflow.com/a/41511598
+			CommonOpenFileDialog dialog = new CommonOpenFileDialog {
+				InitialDirectory = activeDirectoryPath,
+				IsFolderPicker = true
+			};
+			if (dialog.ShowDialog() == CommonFileDialogResult.Ok) {
+				//MessageBox.Show("You selected: " + dialog.FileName);
+				SourceDirectory.Text = dialog.FileName;
+				activeDirectoryPath = dialog.FileName;
+			}
+
+			PopulateFolderListbox(activeDirectoryPath);
+		}
 
 
-			// Add the new list items
-			foreach (string dir in folders) {
-				list_Folders.Items.Add(dir.Substring(dir.LastIndexOf("\\") + 1));
+
+		/// <summary>
+		/// Opens a folder picker dialog to choose the destination directory. The output packed dat file is saved inside of this folder.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void BrowseDestinationDirectory_Click(object sender, RoutedEventArgs e) {
+			CommonOpenFileDialog dialog = new CommonOpenFileDialog {
+				InitialDirectory = destinationDirectoryPath,
+				IsFolderPicker = true
+			};
+			if (dialog.ShowDialog() == CommonFileDialogResult.Ok) {
+				DestinationDirectory.Text = dialog.FileName;
+				destinationDirectoryPath = dialog.FileName;
 			}
 		}
 
-		private void BrowseDestinationDirectory_Click(object sender, RoutedEventArgs e) {
 
+
+		/// <summary>
+		/// Lists all folders in the specified path in FolderList listBox
+		/// </summary>
+		/// <param name="path"></param>
+		private void PopulateFolderListbox(string path) {
+			string[] folders = Directory.GetDirectories(path);
+
+			// Remove all existing list items
+			foreach (string item in FolderList.Items) {
+				FolderList.Items.Remove(item);
+			}
+
+			// Add the new list items from the source directory
+			foreach (string dir in folders) {
+				FolderList.Items.Add(dir.Substring(dir.LastIndexOf("\\") + 1));
+			}
+		}
+
+
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void Pack_Click(object sender, RoutedEventArgs e) {
+
+
+			if (FolderList.SelectedItems.Count == 0) {
+				System.Diagnostics.Debug.WriteLine("zero items selected. doing nothing.");
+				return;
+			}
+			System.Diagnostics.Debug.WriteLine("more than zero items selected. continuing...");
+
+
+
+			List<string> allFoldersToSearch = new List<string>();
+			foreach (string folder in FolderList.SelectedItems) {
+				allFoldersToSearch.Add(activeDirectoryPath + "\\" + folder);
+				System.Diagnostics.Debug.WriteLine("root folder added: " + activeDirectoryPath + "\\" + folder);
+
+				//get list of each sub folder inside the selected folder
+				string[] subfolders = Directory.GetDirectories(activeDirectoryPath + "\\" + folder, "*", SearchOption.AllDirectories);
+				//loop over the list of subfolders and add them to the master search list
+				foreach (string subfolder in subfolders) {
+					allFoldersToSearch.Add(subfolder);
+					System.Diagnostics.Debug.WriteLine("subfolder added: " + subfolder);
+				}
+			}
+
+		}
+
+
+
+		/// <summary>
+		/// Quits the application.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void Quit_Click(object sender, RoutedEventArgs e) {
+			Application.Current.Shutdown();
 		}
 	}
 }
