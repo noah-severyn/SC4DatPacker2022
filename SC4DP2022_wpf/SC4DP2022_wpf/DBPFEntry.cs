05a39cb71f2b38bac493c3cd38b7d1665e3bb95f
noah-severyn <50927852+noah-severyn@users.noreply.github.com>
2022-01-17 15:07:10 -0500

    Create DBPFEntry.cs

---
+++ SC4DP2022_wpf/SC4DP2022_wpf/DBPFEntry.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+//See: https://github.com/memo33/jDBPFX/blob/master/src/jdbpfx/DBPFEntry.java
+namespace SC4DP2022_wpf {
+	/// <summary>
+	/// An absract form on an entry of a DBPFFile, representing an instance of a subfile that may be contained in a DBPF file
+	/// </summary>
+	class DBPFEntry {
+		private DBPFTGI _tgi;
+		public DBPFTGI TGI {
+			get { return _tgi; }
+			set {
+				if (value == null) {
+					throw new Exception("Null TGI");
+				} else {
+					_tgi = value;
+				}
+				
+			}
+		}
+
+		// Constructor
+		public DBPFEntry(DBPFTGI tgi) {
+			_tgi = tgi;
+		}
+
+
+	}
+}
