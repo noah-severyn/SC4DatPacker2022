22e847e7e63dbc06b48274f6642b1cd92a80c25e
noah-severyn <50927852+noah-severyn@users.noreply.github.com>
2022-01-17 14:05:05 -0500

    redid Header class (again)

--- SC4DP2022_wpf/SC4DP2022_wpf/DBPF.cs
+++ SC4DP2022_wpf/SC4DP2022_wpf/DBPF.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Text;
 using System.Linq;
 using System.IO;
@@ -7,178 +8,122 @@ using System.Diagnostics;
 
 namespace SC4DP2022_wpf {
 	public class DBPFFile {
+		public Header header; //TODO - does this need to be sealed? How to modify Header to allow this?
+		public FileInfo file; //TODO - this probably needs to be replaced with a file type of something like 'File' later on
+		public OrderedDictionary entryMap; //TODO - implement entrymap
+		//TODO - implement tgimap
 
 
 		public class Header {
-			private uint identifier;
-			private uint majorVersion;
-			private uint minorVersion;
-			private uint dateCreated;
-			private uint dateModified;
-			private uint indexMajorVersion;
-			private uint indexEntryCount;
-			private uint indexEntryOffset;
-			private uint indexSize;
+			private uint _identifier;
+			private uint _majorVersion;
+			private uint _minorVersion;
+			private uint _dateCreated;
+			private uint _dateModified;
+			private uint _indexMajorVersion;
+			private uint _indexEntryCount;
+			private uint _indexEntryOffset;
+			private uint _indexSize;
+			public uint identifier {
+				get { return _identifier; }
+				set {
+					uint identifierDbpf = (uint) 0x44425046; //1145196614 dec = 44425046 hex = DBPF ascii
+					if (value.CompareTo(identifierDbpf) != 0) {
+						throw new Exception("File is not a DBPF file!");
+					}
+					else {
+						_identifier = value;
+					}
+				}
+			}
+			public uint majorVersion {
+				get { return _majorVersion; }
+				set {
+					if (value != (uint) 0x1000000) { //16777216 dec = 1000000 hex
+						throw new Exception("Unsupported major.minor version. Only 1.0 is supported for SC4 DBPF files.");
+					}
+					else {
+						_majorVersion = value;
+					}
+				}
+			}
+			public uint minorVersion {
+				get { return _minorVersion; }
+				set {
+					if (value != (uint) 0) {
+						throw new Exception("Unsupported major.minor version. Only 1.0 is supported for SC4 DBPF files.");
+					}
+					else {
+						_minorVersion = value;
+					}
+				}
+			}
+			public uint dateCreated {
+				get { return _dateCreated; }
+				set { _dateCreated = value; }
+			}
+			public uint dateModified {
+				get { return _dateModified; }
+				set { _dateModified = value; }
+			}
+			public uint indexMajorVersion {
+				get { return _indexMajorVersion; }
+				set {
+					if (value != (uint) 0x7000000) { //117440512 dec = 7000000 hex
+						throw new Exception("Unsupported major.minor version. Only 1.0 is supported for SC4 DBPF files.");
+					}
+					else {
+						_indexMajorVersion = value;
+					}
+				}
+			}
+			public uint indexEntryCount {
+				get { return _indexEntryCount; }
+				set { _indexEntryCount = value; }
+			}
+			public uint indexEntryOffset {
+				get { return _indexEntryOffset; }
+				set { _indexEntryOffset = value; }
+			}
+			public uint indexSize {
+				get { return _indexSize; }
+				set { _indexSize = value; }
+			}
 
 			//empty constructor to prevent automatic creation of blank constructor and assigning default values to fields
-			private Header() { }
+			public Header() { }
 
-
-			public uint GetMajorVersion() {
-				return majorVersion;
-			}
-			public uint GetMinorVersion() {
-				return minorVersion;
-			}
-			public uint GetDateCreated() {
-				return dateCreated;
-			}
-			public uint GetDateModified() {
-				return dateModified;
-			}
-			public uint GetIndexMajorVersion() {
-				return indexMajorVersion;
-			}
-			public uint GetIndexEntryCount() {
-				return indexEntryCount;
-			}
-			public uint GetIndexOffsetLocation() {
-				return indexEntryOffset;
-			}
-			public uint GetIndexSize() {
-				return indexSize;
-			}
-
-			public override string ToString() {
-				StringBuilder sb = new StringBuilder();
-				sb.Append($"Version: {majorVersion}.{minorVersion}; ");
-				sb.Append($"Created: {dateCreated}; ");
-				sb.Append($"Modified: {dateModified}; ");
-				sb.Append($"Index Major Version: {indexMajorVersion}; ");
-				sb.Append($"Index Entry Count: {indexEntryCount}; ");
-				sb.Append($"Index Offset Location: {indexEntryOffset}; ");
-				sb.Append($"Index Size: {indexSize}; ");
-				return sb.ToString();
-
-			}
-
-
-
-
-
-
-
-
+			//public override string ToString() {
+			//	StringBuilder sb = new StringBuilder();
+			//	sb.Append($"Version: {majorVersion}.{minorVersion}; ");
+			//	sb.Append($"Created: {dateCreated}; "); // TODO - add functions to output these in a readable format - possibly functions in a util class?
+			//	sb.Append($"Modified: {dateModified}; "); // TODO - add functions to output these in a readable format - possibly functions in a util class?
+			//	sb.Append($"Index Major Version: {indexMajorVersion}; ");
+			//	sb.Append($"Index Entry Count: {indexEntryCount}; ");
+			//	sb.Append($"Index Offset Location: {indexEntryOffset}; ");
+			//	sb.Append($"Index Size: {indexSize}; ");
+			//	return sb.ToString();
+			//}
 		}
 
-		//public uint HeaderIdentifier {
-		//	get { return identifier; }
-		//	set {
-		//		uint identifierDbpf = (uint) 0x44425046; //1145196614 dec = 44425046 hex = DBPF ascii
-		//		if (value.CompareTo(identifierDbpf) != 0) {
-		//			throw new Exception("File is not a DBPF file!");
-		//		}
-		//		else {
-		//			identifier = value;
-		//		}
-		//	}
-		//}
 
-
-		//public uint HeaderMajorVersion {
-		//	get { return majorVersion; }
-		//	set {
-		//		if (value != (uint) 0x1000000) { //16777216 dec = 1000000 hex
-		//			throw new Exception("Unsupported major.minor version. Only 1.0 is supported for SC4 DBPF files.");
-		//		}
-		//		else {
-		//			majorVersion = value;
-		//		}
-		//	}
-		//}
-
-
-		//public uint HeaderMinorVersion {
-		//	get { return minorVersion; }
-		//	set {
-		//		if (value != (uint) 0) {
-		//			throw new Exception("Unsupported major.minor version. Only 1.0 is supported for SC4 DBPF files.");
-		//		}
-		//		else {
-		//			minorVersion = value;
-		//		}
-		//		minorVersion = value;
-		//	}
-		//}
-
-
-		//public uint HeaderDateCreated {
-		//	get { return dateCreated; }
-		//	set { dateCreated = value; }
-		//}
-
-
-		//public uint HeaderDateModified {
-		//	get { return dateModified; }
-		//	set { dateModified = value; }
-		//}
-
-
-		//public uint HeaderIndexMajorVersion {
-		//	get { return indexMajorVersion; }
-		//	set {
-		//		if (value != (uint) 0x7000000) { //117440512 dec = 7000000 hex
-		//			throw new Exception("Unsupported major.minor version. Only 1.0 is supported for SC4 DBPF files.");
-		//		}
-		//		else {
-		//			indexMajorVersion = value;
-		//		}
-		//	}
-		//}
-
-
-		// Default Constructor
-		//public DBPFHeader() {
-		//identifier = (uint) 44425046; //DBPF in hex
-		//majorVersion = (uint) 1;
-		//minorVersion = (uint) 0;
-		//dateCreated = (uint) DateTimeOffset.Now.ToUnixTimeSeconds();
-		//dateModified = (uint) DateTimeOffset.Now.ToUnixTimeSeconds();
-		//indexMajorVersion = (uint) 7;
-		////indexEntryCount;
-		////indexEntryOffset;
-		////indexSize;
-		//holeEntryCount = (uint) 0;
-		//holeOffset = (uint) 0;
-		//holeSize = (uint) 0;
-		//indexMinorVersion = (uint) 0;
-		//}
-
-		// Constructor with supplied values
-		//public DBPFHeader(uint[] headerInfo) {
-		//	//check if input is the proper length of 96 bytes
-		//	if (headerInfo.Length != 18) {
-		//		throw new Exception($"Incorrect header size supplied to the DBPFHeader class. Provided {headerInfo.Length}, expected 18.");
-		//	}
-		//	identifier = headerInfo[0];
-		//	majorVersion = headerInfo[1];
-		//	minorVersion = headerInfo[2];
-		//	dateCreated = headerInfo[6];
-		//	dateModified = headerInfo[7];
-		//	indexMajorVersion = headerInfo[8];
-		//	indexEntryCount = headerInfo[9];
-		//	indexEntryOffset = headerInfo[10];
-		//	indexSize = headerInfo[11];
-		//	holeEntryCount = headerInfo[12];
-		//	holeOffset = headerInfo[13];
-		//	holeSize = headerInfo[14];
-		//	indexMinorVersion = headerInfo[15];
+		//Constructor with supplied values
+		//public DBPFFile(string fileName, uint majorVersion, uint minorVersion, uint dateCreated, uint dateModified, uint indexMajorVersion, uint indexEntryCount, uint indexOffsetLocation, uint indexSize) {  //TODO - change constructor to private
+		//	this.file = fileName;
+		//	this.header = new Header();
+		//	this.header.majorVersion = majorVersion;
+		//	this.header.minorVersion = minorVersion;
+		//	this.header.dateCreated = dateCreated;
+		//	this.header.dateModified = dateModified;
+		//	this.header.indexMajorVersion = indexMajorVersion;
+		//	this.header.indexEntryCount = indexEntryCount;
+		//	this.header.indexEntryOffset = indexOffsetLocation;
+		//	this.header.indexSize = indexSize;
 		//}
 
 		public override string ToString() { //TODO : Implement this?
 			return base.ToString();
 		}
-		//}
 
 
 
@@ -189,34 +134,61 @@ namespace SC4DP2022_wpf {
 
 
 		public DBPFFile(string filePath) {
-			//read first 96 bytes of file
-			byte[] headerBytes = new byte[96];
-			FileStream fs = new FileStream(filePath, FileMode.Open);
-			BinaryReader reader = new BinaryReader(fs);
-			reader.BaseStream.Read(headerBytes, 0, 96);
+			////read first 96 bytes of file
+			//byte[] headerBytes = new byte[96];
+			//FileStream fs = new FileStream(filePath, FileMode.Open);
+			//BinaryReader reader = new BinaryReader(fs);
+			//reader.BaseStream.Read(headerBytes, 0, 96);
 
-			// https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/types/how-to-convert-a-byte-array-to-an-int
-			if (BitConverter.IsLittleEndian) {
-				Array.Reverse(headerBytes);
-			}
-			//DBPFHeader header = new DBPFHeader();
-			this.HeaderIdentifier = BitConverter.ToUInt32(headerBytes, 92); //1145196614 int = 44425046 hex = DBPF ascii
-			System.Diagnostics.Debug.WriteLine("HeaderIdentifier: " + this.HeaderIdentifier);
-			this.HeaderMajorVersion = BitConverter.ToUInt32(headerBytes, 88);
-			System.Diagnostics.Debug.WriteLine("HeaderMajorVersion: " + this.HeaderMajorVersion);
-			this.HeaderMinorVersion = BitConverter.ToUInt32(headerBytes, 84);
-			System.Diagnostics.Debug.WriteLine("HeaderMinorVersion: " + this.HeaderMinorVersion);
-			this.HeaderDateCreated = BitConverter.ToUInt32(headerBytes, 68);
-			this.HeaderDateModified = BitConverter.ToUInt32(headerBytes, 64);
-			this.HeaderIndexMajorVersion = BitConverter.ToUInt32(headerBytes, 60);
-			this.HeaderIndexEntryCount = BitConverter.ToUInt32(headerBytes, 56);
-			this.HeaderIndexEntryOffset = BitConverter.ToUInt32(headerBytes, 52);
-			this.HeaderIndexSize = BitConverter.ToUInt32(headerBytes, 48);
-			this.HeaderHoleEntryCount = BitConverter.ToUInt32(headerBytes, 44);
-			this.HeaderHoleOffset = BitConverter.ToUInt32(headerBytes, 40);
-			this.HeaderHoleSize = BitConverter.ToUInt32(headerBytes, 36);
-			this.HeaderIndexMinorVersion = BitConverter.ToUInt32(headerBytes, 32);
+			//// https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/types/how-to-convert-a-byte-array-to-an-int
+			//if (BitConverter.IsLittleEndian) {
+			//	Array.Reverse(headerBytes);
+			//}
+			//Header header = new Header();
+			//this.header.identifier = BitConverter.ToUInt32(headerBytes, 92); //1145196614 int = 44425046 hex = DBPF ascii
+			//this.header.majorVersion = BitConverter.ToUInt32(headerBytes, 88);
+			//this.header.minorVersion = BitConverter.ToUInt32(headerBytes, 84);
+			//this.header.dateCreated = BitConverter.ToUInt32(headerBytes, 68);
+			//this.header.dateModified = BitConverter.ToUInt32(headerBytes, 64);
+			//this.header.indexMajorVersion = BitConverter.ToUInt32(headerBytes, 60);
+			//this.header.indexEntryCount = BitConverter.ToUInt32(headerBytes, 56);
+			//this.header.indexEntryOffset = BitConverter.ToUInt32(headerBytes, 52);
+			//this.header.indexSize = BitConverter.ToUInt32(headerBytes, 48);
+
+			FileInfo file = new FileInfo(filePath); //probably best to use the FileInfo object to better deal with io errors
+			Header header = new Header();
+			readHeader(file, header);
+
 		}
+
+		public void readHeader(FileInfo inputFile, Header header) {
+			
+			FileStream fs = new FileStream(inputFile.FullName, FileMode.Open);
+			BinaryReader br = new BinaryReader(fs);
+			header.identifier = DBPFUtil.ReverseBytes(br.ReadUInt32());
+			header.majorVersion = DBPFUtil.ReverseBytes(br.ReadUInt32());
+			header.minorVersion = DBPFUtil.ReverseBytes(br.ReadUInt32());
+			br.BaseStream.Seek(12, SeekOrigin.Current); //skip 8 unused bytes
+			header.dateCreated = DBPFUtil.ReverseBytes(br.ReadUInt32());
+			header.dateModified = DBPFUtil.ReverseBytes(br.ReadUInt32());
+			header.indexMajorVersion = DBPFUtil.ReverseBytes(br.ReadUInt32());
+			header.indexEntryCount = DBPFUtil.ReverseBytes(br.ReadUInt32());
+			header.indexEntryOffset = DBPFUtil.ReverseBytes(br.ReadUInt32());
+			header.indexSize = DBPFUtil.ReverseBytes(br.ReadUInt32());
+			br.Close();
+			fs.Close();
+		}
+
+		////https://www.wiki.sc4devotion.com/index.php?title=DBPF
+		//public static bool isDBPF(string filePath) { //TODO - possibly rewrite using the Binary class??? https://docs.microsoft.com/en-us/dotnet/api/system.data.linq.binary?view=netframework-4.8
+		//	byte[] identifierFile = new byte[4];
+		//	byte[] identifierDbpf = new byte[] { 0x44, 0x42, 0x50, 0x46 };
+		//	FileStream fs = new FileStream(filePath, FileMode.Open);
+		//	BinaryReader reader = new BinaryReader(fs);
+		//	reader.BaseStream.Read(identifierFile, 0, 4);
+		//	fs.Close();
+		//	return identifierFile.SequenceEqual(identifierDbpf);
+		//}
 
 	}
 }
