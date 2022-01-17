cf2f5a309aef319e1d0448c13b8ea5594991d7d8
noah-severyn <50927852+noah-severyn@users.noreply.github.com>
2022-01-17 14:18:38 -0500

    update tests for new header nested class

--- SC4DP2022_wpf/SC4DP2022_wpfTests/UnitTest1.cs
+++ SC4DP2022_wpf/SC4DP2022_wpfTests/UnitTest1.cs
@@ -9,10 +9,10 @@ namespace SC4DP2022_wpfTests {
 		public void Test_001_ValidDBPF() {
 			//These should pass : valid DBPF file
 			DBPFFile dbpf = new DBPFFile("C:\\Users\\Administrator\\Documents\\SimCity 4\\Plugins\\mntoes\\Bournemouth Housing Pack\\Mntoes-Bournemouth Housing Pack.dat");
-			Assert.AreEqual((uint) 0x44425046, dbpf.HeaderIdentifier); //1145196614 dec = 44425046 hex = DBPF ascii
-			Assert.AreEqual((uint) 0x1000000, dbpf.HeaderMajorVersion); //16777216 dec = 1000000 hex
-			Assert.AreEqual((uint) 0x0, dbpf.HeaderMinorVersion);
-			Assert.AreEqual((uint) 0x7000000, dbpf.HeaderIndexMajorVersion); //117440512 dec = 7000000 hex)
+			Assert.AreEqual((uint) 0x44425046, dbpf.header.identifier); //1145196614 dec = 44425046 hex = DBPF ascii
+			Assert.AreEqual((uint) 0x1000000, dbpf.header.majorVersion); //16777216 dec = 1000000 hex
+			Assert.AreEqual((uint) 0x0, dbpf.header.minorVersion);
+			Assert.AreEqual((uint) 0x7000000, dbpf.header.indexMajorVersion); //117440512 dec = 7000000 hex)
 			}
 
 		[TestMethod]
