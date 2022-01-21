b02108fbd1d4a8daa320f3ec4bc00b60a896111b
noah-severyn <50927852+noah-severyn@users.noreply.github.com>
2022-01-20 22:53:15 -0500

    Update UnitTest1.cs

--- SC4DP2022_wpf/SC4DP2022_wpfTests/UnitTest1.cs
+++ SC4DP2022_wpf/SC4DP2022_wpfTests/UnitTest1.cs
@@ -8,7 +8,8 @@ namespace SC4DP2022_wpfTests {
 		[TestMethod]
 		public void Test_001_ValidDBPF() {
 			//These should pass : valid DBPF file
-			DBPFFile dbpf = new DBPFFile("C:\\Users\\Administrator\\Documents\\SimCity 4\\Plugins\\mntoes\\Bournemouth Housing Pack\\Mntoes-Bournemouth Housing Pack.dat");
+			//DBPFFile dbpf = new DBPFFile("C:\\Users\\Administrator\\Documents\\SimCity 4\\Plugins\\mntoes\\Bournemouth Housing Pack\\Mntoes-Bournemouth Housing Pack.dat");
+			DBPFFile dbpf = new DBPFFile("C:\\Users\\Administrator\\Documents\\SimCity 4\\Plugins\\z_DataView - Parks Aura.dat");
 			Assert.AreEqual((uint) 0x44425046, dbpf.header.identifier); //1145196614 dec = 44425046 hex = DBPF ascii
 			Assert.AreEqual((uint) 0x1000000, dbpf.header.majorVersion); //16777216 dec = 1000000 hex
 			Assert.AreEqual((uint) 0x0, dbpf.header.minorVersion);
