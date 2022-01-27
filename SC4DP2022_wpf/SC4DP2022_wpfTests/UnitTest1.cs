af974262afeb7f0d73684b997a152547d27a9088
noah-severyn <50927852+noah-severyn@users.noreply.github.com>
2022-01-26 23:04:26 -0500

    added unit tests for DBPFTGI.Equals

--- SC4DP2022_wpf/SC4DP2022_wpfTests/UnitTest1.cs
+++ SC4DP2022_wpf/SC4DP2022_wpfTests/UnitTest1.cs
@@ -31,7 +31,36 @@ namespace SC4DP2022_wpfTests {
 		}//Example: 1697917002 (0x 65 34 28 4A) returns 1244148837 (0x 4A 28 34 65)
 
 		[TestMethod]
-		public void Test_001_ValidDBPF() {
+		public void Test_050_DBPFTGI_Equals() {
+			DBPFTGI tgi1 = new DBPFTGI(0, 0, 0);
+			DBPFTGI tgi2 = new DBPFTGI(0, 0, 0);
+			DBPFTGI tgi3 = new DBPFTGI(0xe86b1eef, 0xe86b1eef, 0x286b1f03);
+			DBPFTGI tgi4 = new DBPFTGI(3899334383, 3899334383, 678108931);
+			Assert.AreEqual(tgi1, tgi2);
+			Assert.IsTrue(tgi1.Equals(tgi2));
+			Assert.IsTrue(tgi2.Equals(tgi1));
+			Assert.AreEqual(tgi3, tgi4);
+			Assert.IsTrue(tgi3.Equals(tgi4));
+			Assert.IsTrue(tgi4.Equals(tgi3));
+
+			Assert.AreNotEqual(tgi2, tgi3);
+			Assert.IsFalse(tgi1.Equals(tgi3));
+			Assert.IsFalse(tgi3.Equals(tgi1));
+
+			Assert.IsFalse(tgi1.Equals("string"));
+			DBPFEntry e1 = new DBPFEntry(tgi1, 0, 0, 0);
+			Assert.IsFalse(tgi1.Equals(e1));
+		}
+
+		[TestMethod]
+		public void Test_051_DBPFTGI_Matches() {
+
+		}
+
+
+
+		[TestMethod]
+		public void Test_101_ValidDBPF() {
 			//These should pass : valid DBPF file
 			//DBPFFile dbpf = new DBPFFile("C:\\Users\\Administrator\\Documents\\SimCity 4\\Plugins\\mntoes\\Bournemouth Housing Pack\\Mntoes-Bournemouth Housing Pack.dat");
 			DBPFFile dbpf = new DBPFFile("C:\\Users\\Administrator\\Documents\\SimCity 4\\Plugins\\z_DataView - Parks Aura.dat");
@@ -43,7 +72,7 @@ namespace SC4DP2022_wpfTests {
 
 		[TestMethod]
 		[Ignore]
-		public void Test_020_NotValidDBPF() {
+		public void Test_102_NotValidDBPF() {
 			//These should fail : not valid DBPF file
 			Exception ex = Assert.ThrowsException<Exception>(() => new DBPFFile("C:\\Users\\Administrator\\Documents\\SimCity 4\\Plugins\\CAS_AutoHistorical_v0.0.2.dll"));
 			Assert.IsTrue(ex.Message.Contains("File is not a DBPF file!"));
