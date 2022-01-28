0a05ace8f1bd9d78ff8272cde25e7d4e201f5b8e
noah-severyn <50927852+noah-severyn@users.noreply.github.com>
2022-01-27 21:59:50 -0500

    implement known TGI types dictionary

--- SC4DP2022_wpf/SC4DP2022_wpfTests/DBPFUnitTests.cs
+++ SC4DP2022_wpf/SC4DP2022_wpfTests/DBPFUnitTests.cs
@@ -6,7 +6,6 @@ namespace SC4DP2022_wpfTests {
 	[TestClass]
 	public class DBPFUnitTests {
 
-
 		[TestMethod]
 		public void Test_011_DBPFUtil_ReverseBytes() {
 			//Example: 1697917002 (0x 65 34 28 4A) returns 1244148837 (0x 4A 28 34 65)
@@ -49,7 +48,7 @@ namespace SC4DP2022_wpfTests {
 
 		[TestMethod]
 		public void Test_051_DBPFTGI_Matches() {
-
+			Assert.IsTrue(false);
 		}
 
 
@@ -60,9 +59,9 @@ namespace SC4DP2022_wpfTests {
 			//DBPFFile dbpf = new DBPFFile("C:\\Users\\Administrator\\Documents\\SimCity 4\\Plugins\\mntoes\\Bournemouth Housing Pack\\Mntoes-Bournemouth Housing Pack.dat");
 			DBPFFile dbpf = new DBPFFile("C:\\Users\\Administrator\\Documents\\SimCity 4\\Plugins\\z_DataView - Parks Aura.dat");
 			Assert.AreEqual((uint) 0x44425046, dbpf.header.identifier); //1145196614 dec = 44425046 hex = DBPF ascii
-			Assert.AreEqual((uint) 0x1000000, dbpf.header.majorVersion); //16777216 dec = 1000000 hex
-			Assert.AreEqual((uint) 0x0, dbpf.header.minorVersion);
-			Assert.AreEqual((uint) 0x7000000, dbpf.header.indexMajorVersion); //117440512 dec = 7000000 hex)
+			Assert.AreEqual((uint) DBPFUtil.ReverseBytes(1), dbpf.header.majorVersion); //16777216 dec = 1000000 hex
+			Assert.AreEqual((uint) 0, dbpf.header.minorVersion);
+			Assert.AreEqual((uint) DBPFUtil.ReverseBytes(7), dbpf.header.indexMajorVersion); //117440512 dec = 7000000 hex)
 		}
 
 		[TestMethod]
@@ -72,20 +71,18 @@ namespace SC4DP2022_wpfTests {
 			Exception ex = Assert.ThrowsException<Exception>(() => new DBPFFile("C:\\Users\\Administrator\\Documents\\SimCity 4\\Plugins\\CAS_AutoHistorical_v0.0.2.dll"));
 			Assert.IsTrue(ex.Message.Contains("File is not a DBPF file!"));
 
-
-
 		}
 
 
 		[TestMethod]
 		public void Test_110_ParseHeader() {
-
+			Assert.IsTrue(false);
 		}
 
 
 		[TestMethod]
 		public void Test_210_ParseIndex() {
-
+			Assert.IsTrue(false);
 		}
 	}
 
